7db1e809dd89910f103dd1a21faf4c67879d600e
mp5maker <khan.photon@gmail.com>
2023-12-18 14:56:35 +0600

    :sparkles: feat: added new feature to generate swagger in json and yml format

--- swaggerCli/index.js
+++ swaggerCli/index.js
@@ -1,7 +1,11 @@
+#!/usr/bin/env node
 const fs = require("fs");
 const util = require("util");
 const { convert } = require("./swaggerGen");
 const convertYamlToDocstring = require("./convertYamlToDocstring");
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
 const fpRead = util.promisify(fs.readFile);
 const fpWrite = util.promisify(fs.writeFile);
@@ -9,11 +13,11 @@ const fpWrite = util.promisify(fs.writeFile);
 const performAction = async () => {
   const inJSON = await fpRead("./input.json");
   const configuration = {
-    noInt: false,
-    requestExamples: false,
-    nullType: "string",
-    yamlOut: true,
-    docstring: false,
+    noInt: argv.noInt === 'y' ? true : false,
+    requestExamples: argv.requestExamples === 'y' ? true : false,
+    nullType: typeof argv.nullType === 'string' ? argv.nullType : 'string',
+    yamlOut: argv.yamlOut === 'y' ? argv.yamlOut : false,
+    docstring: argv.docstring === 'y' ? argv.docstring : false,
   };
   const output = convert({ inJSON, configuration });
   const confirmedOutput = configuration.docstring
