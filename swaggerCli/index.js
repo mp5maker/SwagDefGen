30dc1d964315f0a0119afc2917c134fdad9b9298
mp5maker <khan.photon@gmail.com>
2023-12-20 20:36:48 +0600

    :sparkles: feat: updated the logic for the swagger cli

--- swaggerCli/index.js
+++ swaggerCli/index.js
@@ -3,30 +3,37 @@ const fs = require("fs");
 const util = require("util");
 const { convert } = require("./swaggerGen");
 const convertYamlToDocstring = require("./convertYamlToDocstring");
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
 
 const fpRead = util.promisify(fs.readFile);
 const fpWrite = util.promisify(fs.writeFile);
 
 const performAction = async () => {
   const inJSON = await fpRead("./input.json");
+
   const configuration = {
-    noInt: argv.noInt === 'y' ? true : false,
-    requestExamples: argv.requestExamples === 'y' ? true : false,
-    nullType: typeof argv.nullType === 'string' ? argv.nullType : 'string',
-    yamlOut: argv.yamlOut === 'y' ? argv.yamlOut : false,
-    docstring: argv.docstring === 'y' ? argv.docstring : false,
+    noInt: argv.noInt === "y" ? true : false,
+    requestExamples: argv.requestExamples === "y" ? true : false,
+    nullType: typeof argv.nullType === "string" ? argv.nullType : "string",
+    yamlOut: argv.yamlOut === "y" ? argv.yamlOut : false,
+    docstring: argv.docstring === "y" ? argv.docstring : false,
+    forcedSpace: argv.forcedSpace ? argv.forcedSpace : 0,
   };
+
+  const outputPath = `./output.${configuration.yamlOut ? "yml" : "json"}`;
   const output = convert({ inJSON, configuration });
-  const confirmedOutput = configuration.docstring
-    ? convertYamlToDocstring(output)
+  const splittedOutput = output.split("\n")
+  const outputForcedSpaced = configuration.forcedSpace
+    ? splittedOutput.map((line, index) => {
+        return " ".repeat(configuration.forcedSpace) + line;
+      }).join('\n')
     : output;
-  await fpWrite(
-    `./output.${configuration.yamlOut ? "yml" : "json"}`,
-    confirmedOutput
-  );
+  const outputDocstring = configuration.docstring
+    ? convertYamlToDocstring(outputForcedSpaced)
+    : outputForcedSpaced;
+  await fpWrite(outputPath, outputDocstring);
 };
 
 performAction();
